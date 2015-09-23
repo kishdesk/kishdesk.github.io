167bad4c1adcd8d2fb60dd1b1e4b028fcb5f3139
kishdesk <skishore1992@gmail.com>
2015-09-23 23:39:16 +0530

    redesign - bk

---
+++ one/index.php
@@ -0,0 +1,47 @@
+<html>
+	<head>
+		<title>Brand Konnect</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/video.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<bg>
+			<div class="homepage-hero-module">
+				<div class="video-container">
+					<div class="filter"></div>
+					<video autoplay loop class="fillWidth">
+						<source src="img/city/MP4/nights.mp4" type="video/mp4" />Your browser does not support the video tag. I suggest you upgrade your browser.
+						<source src="img/city/WEBM/nights.webm" type="video/webm" />Your browser does not support the video tag. I suggest you upgrade your browser.
+					</video>
+					<div class="poster hidden">
+						<!--<img src="PATH_TO_JPEG" alt="">-->
+					</div>
+				</div>
+			</div>
+		</bg>
+		<div id="banner" class = "fl jc ac">
+			The world has no time to pay attention to what not is attracted
+		</div>
+		<content class = "fl flc ac">
+			<header class="fl jc" >
+				<div id="logo" class="fl eg">
+					<img src="img/logont.png" alt="logo" height = "55px">
+				</div>
+				<nav class="eg fl jr ac">
+					<a href="index.php">Home</a>
+					<a href="#contact">Contact Us</a>
+				</nav>
+			</header>
+			<main>
+				
+			</main>
+			<footer>
+				
+			</footer>
+		</content>
+		<script src = "js/jquery.js"></script>
+		<script src = "js/anim.js"></script>
+		<script src = "js/video.js"></script>
+	</body>
+</html>
---
+++ one/js/anim.js
@@ -0,0 +1,31 @@
+
+$(function(){
+	
+	function init(){
+		$('bg')
+		.css({
+		'filter'         : 'blur(5px)',
+		'-webkit-filter' : 'blur(5px)',
+		'-moz-filter'    : 'blur(5px)',
+		'-o-filter'      : 'blur(5px)',
+		'-ms-filter'     : 'blur(5px)'
+		});
+		$("#banner").animate({"opacity":"1"},800,function(){
+			setTimeout(init1, 1500);
+		});
+	}
+	function init1(){
+		$("#banner").animate({"opacity":"0"},800,function(){
+			$("#banner").html("We will help you get magnetize");
+			setTimeout(init2,200);
+		});
+	}
+	function init2(){
+		$("#banner").animate({"opacity":"1"},500);
+		setTimeout(end,1500);
+	}
+	function end(){
+		$("#banner").animate({"opacity":"0"})
+	}
+	setTimeout(init, 1000);
+})
